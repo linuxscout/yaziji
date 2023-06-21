36243b6394d4501687d2b7b5af6f8f338e09cebd
linuxscout <taha_zerrouki@hotmail.com>
2023-06-21 13:00:26 +0100

    update config

--- web/config/yaziji_config.py
+++ web/config/yaziji_config.py
@@ -3,22 +3,36 @@
 #
 #  yaziji_config.py
 #  
+OFFLINE = True
+# ~ OFFLINE = False   # hosted localy
 
-
-# URL HOST
-# For Testing
-URL_HOST_PATH = "http://127.0.0.1/yaziji"
-# For Production
-# ~ URL_HOST_PATH = "http://tahadz.com/yaziji"
-
-#Database config directory
-DB_BASE_PATH = "/var/www/html/yaziji/"
-# Logging file
-LOGGING_CFG_FILE = "/var/www/html/yaziji/web/config/logging.cfg"
-LOGGING_FILE = "/var/www/html/yaziji/web/logs/demo.log"
 # in developement True in production False
-MODE_DEBUG = True
-# ~ MODE_DEBUG = False
+#MODE_DEBUG = True
+MODE_DEBUG = False
+
+if OFFLINE:
+    # URL HOST
+    # For Testing
+    URL_HOST_PATH = "http://127.0.0.1/yaziji"
+    DIR_HOST_PATH = "/var/www/html/yaziji"
+
+    #Database config directory
+    DB_BASE_PATH = "/var/www/html/yaziji/"
+    # Logging file
+    LOGGING_CFG_FILE = DIR_HOST_PATH +"/web/config/logging.cfg"
+    LOGGING_FILE     = DIR_HOST_PATH +"/web/logs/demo.log" 
+else:
+    # For Production
+    URL_HOST_PATH = "https://example.com/yaziji"
+    DIR_HOST_PATH = "~/public_html/cgi-bin"    
+    #Database config directory
+    DB_BASE_PATH = "/var/www/html/yaziji/"
+    MY    
+
+    # Logging file
+    LOGGING_CFG_FILE = DIR_HOST_PATH +"/yazijy/web/config/logging.cfg"
+    LOGGING_FILE = DIR_HOST_PATH +"/yazijy/web/logs/demo.log"      
+
 def main(args):
     return 0
 
