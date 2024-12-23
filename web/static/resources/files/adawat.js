3c9722b4ef51c70df05f49da25e78b1485e39762
linuxscout <taha_zerrouki@hotmail.com>
2024-12-23 15:39:49 +0100

    clean adawat.js and refactor

--- web/static/resources/files/adawat.js
+++ web/static/resources/files/adawat.js
@@ -1,85 +1,5 @@
 
 
-//var random_click = function(e) {
-//    e.preventDefault()
-//    $.getJSON(script + "/ajaxGet", {
-//      text: '',
-//      action: "RandomText"
-//    }, function(data) {
-//      if (data) document.NewForm.InputText.value = data.result;
-//      else document.NewForm.InputText.value = "TZA";
-//    });
-//  }
-
-
-//var csv2data_click = function(e) {
-//        e.preventDefault()
-//    $.getJSON(script + ")s/ajaxGet", {
-//      text: document.NewForm.InputText.value,
-//      action: "CsvToData"
-//    }, function(d) {
-//      $("#result").html("<pre>" + d.result + "</pre>");
-//      //"#result").text(d.time);
-//    });
-//  }
-
-
-
-
-
-//----------Tabs----------------------
-//var more_click = function(e) {
-//      e.preventDefault()
-//    $("#moresection").slideToggle();
-//  }
-
-//  var vocalize_group_click = function(e) {
-//        e.preventDefault()
-//    $("#vocalizesection").slideToggle();
-//    $("#moresection").hide();
-//  }
-
-  //copy result into clipboard
-//  var copy_click = function(e) {
-//        e.preventDefault()
-//    $(".txkl").change();
-//    var $temp = $("<input>");
-//    $("body").append($temp);
-//    $temp.val($("#result").text()).select();
-//    document.execCommand("copy");
-//    $temp.remove();
-//    //document.NewForm.InputText.value = $("#result").text();
-//  }
-
-
-//    var phrase_click = function(e) {
-//        e.preventDefault()
-//    if((document.NewForm.tense.value == "الأمر")&&(document.NewForm.subject.value.indexOf("أنت") == -1))
-//     {
-//         alert("خطأ في الضمير ["+document.NewForm.subject.value+"] غير متطابق مع التصريف في الأمر");
-//     return  0;
-//     }
-//    var prefix = getPrefixPath();
-//    //console.log("phrase_click:"+prefix+"/ajaxGet")
-//    $.getJSON(prefix+ "/ajaxGet", {
-//
-//      text: document.NewForm.subject.value,
-//      action: "phrase",
-//      "subject":document.NewForm.subject.value,
-//      "object":document.NewForm.object.value,
-//      "verb":document.NewForm.verb.value,
-//      "time":document.NewForm.time.value,
-//      "place":document.NewForm.place.value,
-//      "tense":document.NewForm.tense.value,
-//      "voice":document.NewForm.voice.value,
-//      "auxiliary":document.NewForm.auxiliary.value,
-//      "negative":document.NewForm.negative.value,
-//      "phrase_type":document.NewForm.phrase_type.value,
-//    }, function(d) {
-//      $("#result").html("<div class=\'tashkeel\'>" + d.result + "</div>");
-//
-//    });
-//  }
 
 /**
  * Handles the phrase click event to validate input and send data via AJAX.
@@ -127,33 +47,6 @@ const phraseClick = (e) => {
 };
 
 
-//    var sample_click = function(e) {
-//        e.preventDefault()
-//    if((document.NewForm.tense.value == "الأمر")&&(document.NewForm.subject.value.indexOf("أنت") == -1))
-//     {
-//         alert("خطأ في الضمير ["+document.NewForm.subject.value+"] غير متطابق مع التصريف في الأمر");
-//     return  0;
-//     }
-//     var prefix = getPrefixPath();
-//     //~ var prefix = script;
-//    $.getJSON(prefix+ "/ajaxGet", {
-//      text: document.NewForm.subject.value,
-//      action: "sample",
-//      "subject":document.NewForm.subject.value,
-//      "object":document.NewForm.object.value,
-//      "verb":document.NewForm.verb.value,
-//      "time":document.NewForm.time.value,
-//      "place":document.NewForm.place.value,
-//      "tense":document.NewForm.tense.value,
-//      "voice":document.NewForm.voice.value,
-//      "auxiliary":document.NewForm.auxiliary.value,
-//      "negative":document.NewForm.negative.value,
-//      "phrase_type":document.NewForm.phrase_type.value,
-//    }, function(d) {
-//      $("#result").html( d.result);
-//
-//    });
-//  }
 
 /**
  * Handles the sample click event to validate the form and send data via AJAX.
@@ -202,30 +95,6 @@ const sampleClick = (e) => {
 
 
 
-//    var report_click = function(e) {
-//        e.preventDefault()
-//     var prefix = getPrefixPath();
-//     //~ var prefix = script;
-//    $.getJSON(prefix+ "/ajaxGet", {
-//      text: document.NewForm.subject.value,
-//      action: "report",
-//      "subject":document.NewForm.subject.value,
-//      "object":document.NewForm.object.value,
-//      "verb":document.NewForm.verb.value,
-//      "time":document.NewForm.time.value,
-//      "place":document.NewForm.place.value,
-//      "tense":document.NewForm.tense.value,
-//      "voice":document.NewForm.voice.value,
-//      "auxiliary":document.NewForm.auxiliary.value,
-//      "negative":document.NewForm.negative.value,
-//      "phrase_type":document.NewForm.phrase_type.value,
-//      "result":$("#result").text(),
-//    }, function(d) {
-//      $("#result").html(d.result);
-//    alert("شكرا لإبلاغنا بالمشكلة..");
-//    });
-//  }
-
 
 /**
  * Handles the report click event to send form data via AJAX.
@@ -269,47 +138,6 @@ const reportClick = (e) => {
 
 
 
-//    var random_select_click = function(e) {
-//        e.preventDefault()
-//    var item;
-//    // verb
-//    var options = $("#verb > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#verb > option").attr('selected',false).eq(random).attr('selected',true);
-//
-//    // time
-//    var options = $("#time > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#time > option").attr('selected',false).eq(random).attr('selected',true);
-//    // place
-//    var options = $("#place > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#place > option").attr('selected',false).eq(random).attr('selected',true);
-//    // negative
-//    var options = $("#negative > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#negative > option").attr('selected',false).eq(random).attr('selected',true);
-//    // auxiliary
-//    var options = $("#auxiliary > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#auxiliary > option").attr('selected',false).eq(random).attr('selected',true);
-//    // phrase_type
-//    var options = $("#phrase_type > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#phrase_type > option").attr('selected',false).eq(random).attr('selected',true);
-//      // tense
-//    var options = $("#tense > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#tense > option").attr('selected',false).eq(random).attr('selected',true);
-//    // voice
-//    var options = $("#voice > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#voice > option").attr('selected',false).eq(random).attr('selected',true);
-//    // subject
-//    var options = $("#subject > option");
-//    var random = Math.floor(options.length * (Math.random() % 1));
-//    $("#subject > option").attr('selected',false).eq(random).attr('selected',true);
-//  }
 
 /**
  * Randomly selects an option from a dropdown by its ID.
@@ -345,21 +173,6 @@ const randomSelectClick = (e) => {
     dropdowns.forEach(selectRandomOption);
 };
 
-/*
- * Return prerfix path according to  acutal script and language
- * */
-//function getPrefixPath()
-//{
-//    //~ return script;
-//var lang = getLang();
-//var prefix = script;
-//if(!lang) lang = "ar";
-//// generate prefix path
-//prefix +="/"+lang;
-////console.log("current script path is: "+prefix)
-////console.log("catched locale is: "+lang)
-//return prefix;
-//}
 
 /**
  * Returns the prefix path based on the current script and language.
@@ -380,15 +193,6 @@ const getPrefixPath = () => {
 };
 
 
-///*
-// * Return language param
-// * */
-//function getLang()
-//{
-//const urlParams = new URLSearchParams(window.location.search);
-//var lang = urlParams.get('locale');
-//return lang;
-//}
 
 /**
  * Returns the language parameter from the URL.
@@ -399,124 +203,19 @@ const getLang = () => {
     return urlParams.get('locale') || "ar";
 };
 
-//// load translations from json files
-//function load_languages(url){
-//var lang = getLang();
-//
-//    var translations={}
-//    $.ajax({
-//    url: url+lang+".json", // Path to the JSON file
-//    method: 'GET',
-//    dataType: 'json',
-//    success: function (data) {
-//    translations[lang]=data;
-//    console.log("Translation",data);
-//    },
-//    error: function (xhr, status, error) {
-//    console.error('Error1:', error);
-//    }
-//    });
-//    return translations;
-//}
-//
-//
-//function translate_label(label, lang)
-//{
-//console.log("Translation inside function translate_label", translations)
-//return translations[lang]["web-labels"][label];
-//}
-//
-//function translate_value(field, key, lang)
-//{
-//return translations[lang][field][key];
-//}
-//
-//
-//function draw(){
-//// this function call /selectget api to get data to be display on fields Select inputs
-//// this fields are translated into target language
-//// fill all select fields from translated strings
-//// fill select fields
-//// Catche the current locale
-//
-//var lang = getLang();
-//var prefix = getPrefixPath();
-//
-////if(!lang || lang === "ar")
-////    $("#NewForm").css("direction", "rtl");
-////else
-////    $("#NewForm").css("direction", "ltr");
-////console.log("Select get :"+prefix+"/selectGet")
-////console.log("Translate :"+translations);
-//
-//   $.getJSON(prefix+"/selectGet", {
-//      text: '',
-//      action: "RandomText"
-//    }, function(data) {
-//
-//      if (!data) console.log("No thing, from selectGet");
-//      else
-//      {
-//      //console.log(data);
-//        var selectValues = data;
-////    console.log("SELECT Values" + selectValues);
-////      console.log("SELECT Values fields" + selectValues.fields);
-//     for (x in selectValues.fields)
-//        {
-//        var field = selectValues.fields[x];
-//        //console.log("field", selectValues.fields[x], field);
-//        // show translation
-//        if(lang != "ar")
-//        {
-//        var ar_label = $('#'+field+"_label").text();
-//        var tr_label =  translate_label(ar_label, lang);
-//        //console.log("ar_label", ar_label,"tr",tr_label, selectValues["web-labels"]);
-//        $('#'+field+"_label").append("["+tr_label+"]");
-//        }
-//        // translate values
-//        $.each(selectValues[field], function(key, value) {
-//        if(lang != "ar")
-//        {
-//        var arabic_translated = key +"  ["+translate_value(field,key, lang)+"]";
-//        $('#'+field).append($("<option></option>").attr("value", key).text(arabic_translated));
-//        }
-//        else
-//        {$('#'+field).append($("<option></option>").attr("value", key).text(value));}
-//        });
-//        }
-//
-//       }
-//    });
-//
-//
-//}
-
-//const rating_change = function(e) {
-//        e.preventDefault()
-//     const prefix = getPrefixPath();
-//       const rating = $(this).val();
-//      //~ var prefix = script;
-//      const fromdata= {
-//      text: document.NewForm.subject.value,
-//      action: "rating",
-//      "subject":document.NewForm.subject.value,
-//      "object":document.NewForm.object.value,
-//      "verb":document.NewForm.verb.value,
-//      "time":document.NewForm.time.value,
-//      "place":document.NewForm.place.value,
-//      "tense":document.NewForm.tense.value,
-//      "voice":document.NewForm.voice.value,
-//      "auxiliary":document.NewForm.auxiliary.value,
-//      "negative":document.NewForm.negative.value,
-//      "phrase_type":document.NewForm.phrase_type.value,
-//      "result":$("#result").text(),
-//      "rating":`${rating}`,
-//    }
-//    $.getJSON(prefix+ "/ajaxGet", fromdata, function(d) {
-//      $("#result").html(d.result);
-//    alert("شكرا لتقييم هذه العملية.");
-//    });
-//  }
+/**
+ * Handles the change event for rating input fields.
+ * Sends a GET request to submit the selected rating and other form data to the server.
+ *
+ * @param {Event} e - The event object triggered by the change event.
+ *
+ * Behavior:
+ * - Prevents the default form submission behavior.
+ * - Collects data from the form, including the selected rating and additional metadata.
+ * - Sends the data via an AJAX GET request to the server.
+ * - Updates the result element with the server response.
+ * - Displays a thank-you alert upon successful submission.
+ */
 const ratingChange = function(e) {
     e.preventDefault();
 
@@ -549,17 +248,6 @@ const ratingChange = function(e) {
 };
 
 
-////copy result into clipboard
-//  var copy_click = function(e) {
-//        e.preventDefault()
-//    var $temp = $("<input>");
-//    $("body").append($temp);
-//    $temp.val($("#result").text()).select();
-//    document.execCommand("copy");
-//    $temp.remove();
-//    alert("نسخت البيانات في الحافظة.");
-//    //document.NewForm.InputText.value = $("#result").text();
-//  }
 const copyToClipboard = (e) => {
     e.preventDefault();
 
