b1676749f9e30277888b6b7f5e35be29462d13e5
linuxscout <taha_zerrouki@hotmail.com>
2024-12-26 10:08:05 +0100

    Add unitest for phrase generator

---
+++ tests/test_phrase_generator.py
@@ -0,0 +1,82 @@
+import unittest
+from unittest.mock import MagicMock
+
+from libqutrub.verb_const import TenseImperative,  TensePast, TensePassivePast, TenseFuture
+from libqutrub.verb_const import  ImperativePronouns, PronounsTable
+
+import sys, os
+# Local libraries
+sys.path.append(os.path.join(os.path.dirname(__file__), "./lib"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../yaziji"))
+
+from yaziji.yaziji_const import VERBAL_PHRASE, NOMINAL_PHRASE
+from yaziji.yaziji_const import ACTIVE_VOICE, PASSIVE_VOICE, AFFIRMATIVE, NEGATIVE
+
+
+from yaziji.phrase_generator import PhraseGenerator
+
+class TestPhraseGenerator(unittest.TestCase):
+    def setUp(self):
+        self.phrase_generator = PhraseGenerator()
+
+
+    # @unittest.skip("Test later")
+    def test_build_error_success(self):
+        # Arrange
+        test_set=[ {"id":1,
+                    "components":{'subject': 'تَاجِرٌ', 'object': '', 'verb': 'سَافَرَ', 'time': 'البَارِحَةَ', 'place': 'طَرِيقٌ',
+                       'tense': TensePast, 'voice': PASSIVE_VOICE, 'auxiliary': 'اِسْتَطَاعَ',
+                       'negative': NEGATIVE, 'phrase_type': NOMINAL_PHRASE},
+                     "phrase":"التَّاجِرُ لَمْ يُسْتَطَعْ أَنْ يُسَافَرَ فِي الطَّرِيقِ البَارِحَةَ",
+                    "valid":True,
+                    },
+                    {"id":1,
+                    "components":{'subject': 'مُهَنْدِسٌ', 'object': '', 'verb': 'اشْتَرَى', 'time': 'أَمْسِ',
+                                  'place': 'سُوقٌ', 'tense': TensePast, 'voice': ACTIVE_VOICE,
+                                  'auxiliary': '', 'negative': NEGATIVE, 'phrase_type': VERBAL_PHRASE},
+                      "phrase":"لَمْ يَشْتَرِ الْمُهَنْدِسُ فِي السُّوقِ أَمْسِ",
+                      "valid":False,
+                    },
+        ]
+        for item in test_set:
+            result = self.phrase_generator.build(item["components"])
+            phrase = result.get("phrase",'')
+            self.assertEqual(phrase == item["phrase"], item["valid"],
+                             msg=f"\nResult  :{result}\nExpected:{item['phrase']}")  # Should return True if preparation is successful
+
+    # @unittest.skip("Test later")
+    def test_build_error_handling(self):
+        # Arrange
+        test_set=[ {"id":1,
+                    "components":{'subject': 'تَاجِرٌ', 'object': '', 'verb': 'سَافَرَ', 'time': 'البَارِحَةَ', 'place': 'طَرِيقٌ',
+                       'tense': TensePast, 'voice': PASSIVE_VOICE, 'auxiliary': 'اِسْتَطَاعَ',
+                       'negative': NEGATIVE, 'phrase_type': NOMINAL_PHRASE},
+                     "phrase":"التَّاجِرُ لَمْ يُسْتَطَعْ أَنْ يُسَافَرَ فِي الطَّرِيقِ البَارِحَةَ",
+                    "valid":True,
+                    },
+                    {"id":1,
+                    "components":{'subject': 'مُهَنْدِسٌ', 'object': '', 'verb': 'اشْتَرَى', 'time': 'أَمْسِ',
+                                  'place': 'سُوقٌ', 'tense': TenseImperative, 'voice': ACTIVE_VOICE,
+                                  'auxiliary': '', 'negative': NEGATIVE, 'phrase_type': VERBAL_PHRASE},
+                      "phrase":"Imperative Tense Incompatible with pronoun.",
+                      "valid":True,
+                    },
+        ]
+        for item in test_set:
+            result = self.phrase_generator.build(item["components"])
+            phrase = result.get("phrase",'')
+            self.assertEqual(phrase == item["phrase"], item["valid"],
+                             msg=f"\nResult  :{result}\nExpected:{item['phrase']}")  # Should return True if preparation is successful
+
+
+    def test_error_message(self):
+        # Assert message error
+        self.assertEqual(self.phrase_generator.error_message(-1), "Imperative Tense Incompatible with pronoun.")
+        self.assertEqual(self.phrase_generator.error_message(-2), "A required name not found.")
+        self.assertEqual(self.phrase_generator.error_message(-3), "Unsupported component key.")
+        self.assertEqual(self.phrase_generator.error_message(-4), "ERROR: Required Phrase type is empty.")
+        self.assertEqual(self.phrase_generator.error_message(999), "Input Error")
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_phrase_pattern.py
+++ tests/test_phrase_pattern.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from libqutrub.verb_const import TenseImperative, ImperativePronouns, PronounsTable, TensePast, TensePassivePast
+from libqutrub.verb_const import TenseImperative,  TensePast, TensePassivePast, TenseFuture
+from libqutrub.verb_const import  ImperativePronouns, PronounsTable
 from pyarabic import araby
 
 import alyahmor.verb_affixer
@@ -14,105 +15,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../yaziji"))
 from yaziji.phrase_pattern import PhrasePattern  # Assuming the class is in phrase_pattern.py
 from yaziji.wordnode import wordNode
 from yaziji.yaziji_const import FATHA_WORD, DAMMA_WORD, KASRA_WORD
-from yaziji.yaziji_const import MARFOU3, MANSOUB, MAJROUR, DEFINED
+from yaziji.yaziji_const import MARFOU3, MANSOUB, MAJROUR, DEFINED, VERBAL_PHRASE, NOMINAL_PHRASE
+from yaziji.yaziji_const import ACTIVE_VOICE, PASSIVE_VOICE, AFFIRMATIVE, NEGATIVE
+from yaziji.yaziji_const import NUMBER_SINGULAR, NUMBER_DUAL, NUMBER_PLURAL, GENDER_FEMALE, GENDER_MALE
 
 class TestPhrasePattern(unittest.TestCase):
     def setUp(self,):
         # Creating instance of PhrasePattern
         self.phrase = PhrasePattern()
     # @unittest.skip("Test later")
-    def test_add_components_valid(self):
-        components_list =[ {"id":1, "components":{'subject': 'هو',
-                                    'object': 'كِتَابٌ',
-                                    'verb': 'قَرَأَ',
-                                    'time': 'كُلَّ يَوْمٍ',
-                                    'place': 'غُرْفَةٌ',
-                                    'tense': 'المضارع المعلوم',
-                                    'voice': 'معلوم',
-                                    'auxiliary': 'أَرَادَ',
-                                    'negative': 'مثبت',
-                                    'phrase_type': 'جملة فعلية'},
-                            "valid":True,
-                    },
-                    {"id": 2, "components": {'subject': 'هو',
-                                            'object': 'كِتَابٌ',
-                                            'verb': 'قَرَأَ',
-                                            'time': 'كُلَّ يَوْمٍ',
-                                            'place': 'غُرْفَةٌ',
-                                            'tense': 'المضارع المعلوم',
-                                            'voice': 'معلوم',
-                                            'auxiliary': 'أَرَادَ',
-                                            'negative': 'مثبت',
-                                            'phrase_type': 'جملة فعلية'},
-                    "valid": True,
-                     },
 
-                    {"id": 3, "components": {'subject': 'هو',
-                                            'object': 'كِتَابٌ',
-                                            'verb': 'قَرَأَ',
-                                            'time': 'كُلَّ يَوْمٍ',
-                                            'place': 'غُرْفَةٌ',
-                                            'tense': 'المضارع المعلوم',
-                                            'voice': 'معلوم',
-                                            'auxiliary': 'أَرَادَ',
-                                            'negative': 'مثبت',
-                                            #'phrase_type': 'جملة فعلية'
-                                              },
-                    "valid": False,
-                    },
-                    {"id": 4, "components": {'subject': 'هو',
-                                            'object': 'كِتَابٌ',
-                                            'verb': 'قَرَأَ',
-                                            'time': 'كُلَّ يَوْمٍ',
-                                            'place': 'غُرْفَةٌ',
-                                            'tense': 'المضارع المعلوم',
-                                            'voice': 'معلوم',
-                                            'auxiliary': 'أَرَادَ',
-                                            'negative': 'مثبت',
-                                            'phrase_type': 'جملة فعلية',
-                                            'wrong_name': 'حقل غير معروف'
-                                              },
-                    "valid": False,
-                    },
-            ]
-        for item in components_list:
-            result = self.phrase.add_components(item["components"])
-            self.assertEqual(result > 0,item["valid"], msg=f"add components \nnames{self.phrase.nodes}\nfeatures{self.phrase.phrase_features}")  # Should return True if preparation is successful
-
-    # @unittest.skip("Test later")
-    def test_prepare_valid(self):
-        components_list =[ {"id":1, "components":{'subject': 'هو',
-                                    'object': 'كِتَابٌ',
-                                    'verb': 'قَرَأَ',
-                                    'time': 'كُلَّ يَوْمٍ',
-                                    'place': 'غُرْفَةٌ',
-                                    'tense': 'المضارع المعلوم',
-                                    'voice': 'معلوم',
-                                    'auxiliary': 'أَرَادَ',
-                                    'negative': 'مثبت',
-                                    'phrase_type': 'جملة فعلية'},
-                            "valid":True,
-                    },
-                    {"id": 2, "components": {'subject': 'هو',
-                                            'object': 'كِتَابٌ',
-                                            'verb': 'قَرَأَ',
-                                            'time': 'كُلَّ يَوْمٍ',
-                                            'place': 'غُرْفَةٌ',
-                                            'tense': 'المضارع المعلوم',
-                                            'voice': 'معلوم',
-                                            'auxiliary': 'أَرَادَ',
-                                            'negative': 'مثبت',
-                                            'phrase_type': 'جملة فعلية'},
-                    "valid": True,
-                    },
-            ]
-
-
-
-        for item in components_list:
-            self.phrase.add_components(item["components"])
-            result = self.phrase.prepare()
-            self.assertTrue(result)  # Should return True if preparation is successful
 
     # @unittest.skip("Test later")
     def test_check_compatibles_invalid(self):
@@ -139,43 +51,19 @@ class TestPhrasePattern(unittest.TestCase):
             self.assertTrue(result, msg=f"الزمن {tense}  متوافق مع الضمير {pronoun}")  # Should return True if compatible
         #TODO: Add more imcompatible cases
 
-    # @unittest.skip("Test later")
-    def test_build(self):
-        test_set=[ {"id":1,
-                    "components":{'subject': 'تَاجِرٌ', 'object': '', 'verb': 'سَافَرَ', 'time': 'البَارِحَةَ', 'place': 'طَرِيقٌ',
-                       'tense': 'الماضي المعلوم', 'voice': 'مبني للمجهول', 'auxiliary': 'اِسْتَطَاعَ',
-                       'negative': 'منفي', 'phrase_type': 'جملة اسمية'},
-                     "phrase":"التَّاجِرُ لَمْ يُسْتَطَعْ أَنْ يُسَافَرَ فِي الطَّرِيقِ البَارِحَةَ",
-                    "valid":True,
-                    },
-                    {"id":1,
-                    "components":{'subject': 'مُهَنْدِسٌ', 'object': '', 'verb': 'اشْتَرَى', 'time': 'أَمْسِ',
-                                  'place': 'سُوقٌ', 'tense': 'الماضي المعلوم', 'voice': 'معلوم',
-                                  'auxiliary': '', 'negative': 'منفي', 'phrase_type': 'جملة فعلية'},
-                      "phrase":"لَمْ يَشْتَرِ الْمُهَنْدِسُ فِي السُّوقِ أَمْسِ",
-                      "valid":True,
-                    },
-        ]
-        for item in test_set:
-            self.phrase.add_components(item["components"])
-            self.phrase.prepare()
-            result = self.phrase.build()
-            self.assertEqual(result == item["phrase"], item["valid"],
-                            msg= f"\nResult  :{result}\nExpected:{item['phrase']}")  # Should return True if preparation is successful
-
     def test_conjugate_noun_by_tags(self):
         # Ensure that the method is callable and runs as expected
         test_set = [
             #valid cases
             {"word":"كِتَابٌ", "conjugated":'الْكِتَابُ',
              "tags":[MARFOU3, DEFINED,],
-             "number":"مفرد", "gender":"مذكر", "defined":False, "valid":True,
+             "number":NUMBER_SINGULAR, "gender":GENDER_MALE, "defined":False, "valid":True,
              },
 
             #Invalid cases
             {"word": "أَحْمَدُ", "conjugated": 'الْأَحْمَدُ',
              "tags": [MARFOU3, DEFINED, ],
-             "number": "مفرد", "gender": "مذكر", "defined": True, "valid": False,
+             "number": NUMBER_SINGULAR, "gender": GENDER_MALE, "defined": True, "valid": False,
              },
         ]
         for item in test_set:
@@ -192,14 +80,14 @@ class TestPhrasePattern(unittest.TestCase):
         test_set = [
             { "word":"كِتاب", #kitab
               "feminin":False,
-              "number":"مفرد",
+              "number":NUMBER_SINGULAR,
                     "pronoun":'هو',  "valid":True,
                     "pronoun_aux":'هو', "valid_aux":True,
                     "tense_verb":TensePast, "tense_aux": TensePast
             },
             { "word":"أم", #kitab
               "feminin": True,
-              "number": "مفرد",
+              "number": NUMBER_SINGULAR,
                     "pronoun":'هي',  "valid":True,
                     "pronoun_aux":'هي', "valid_aux":True,
                     "tense_verb":TensePast, "tense_aux": TensePast
@@ -207,21 +95,21 @@ class TestPhrasePattern(unittest.TestCase):
 
             { "word":"أم", #kitab
               "feminin": True,
-              "number": "مثنى",
+              "number": NUMBER_DUAL,
                     "pronoun":'هما مؤ',  "valid":True,
                     "pronoun_aux":'هما مؤ', "valid_aux":True,
                     "tense_verb":TensePast, "tense_aux": TensePast
             },
             { "word":"أم", #kitab
               "feminin": True,
-              "number": "جمع",
+              "number": NUMBER_PLURAL,
                     "pronoun":'هن',  "valid":True,
                     "pronoun_aux":'هن', "valid_aux":True,
                     "tense_verb":TensePast, "tense_aux": TensePast
             },
             { "word":"أم", #kitab
               "feminin": True,
-              "number": "جمع",
+              "number": NUMBER_PLURAL,
                     "pronoun":'هن',  "valid":True,
                     "pronoun_aux":'هي', "valid_aux":True,
                     "tense_verb":TensePast, "tense_aux": TensePassivePast
@@ -229,7 +117,7 @@ class TestPhrasePattern(unittest.TestCase):
         ]
         for item in test_set:
             wd = wordNode("subject", item["word"])
-            item["gender"] = "مؤنث" if item["feminin"] else "مذكر"
+            item["gender"] = GENDER_FEMALE if item["feminin"] else GENDER_MALE
             wd.update(item)
             pronoun, pronoun_aux = self.phrase.get_pronoun(wd, item["tense_verb"] ,item["tense_aux"],
                                                            # feminin=item["feminin"],
@@ -244,17 +132,16 @@ class TestPhrasePattern(unittest.TestCase):
 
 
     def test_get_noun_attributes(self,):
-        # Mocking noun dictionary lookup to return predefined values
-        # MockLookup.return_value = [{"vocalized": "كِتَابٌ"}]
+
         test_set = [
             {"word" :"كِتاب", #kitab
-            "vocalized": "كِتَابٌ","number":"مفرد","gender":"مذكر", "defined":False, "valid":True,
+            "vocalized": "كِتَابٌ","number":NUMBER_SINGULAR,"gender":GENDER_MALE, "defined":False, "valid":True,
              },
             {"word" :"طاولة", #kitab
-            "vocalized": "طَاوِلَةٌ","number":"مفرد","gender":"مؤنث",  "defined":False,"valid":True,
+            "vocalized": "طَاوِلَةٌ","number":NUMBER_SINGULAR,"gender":GENDER_FEMALE,  "defined":False,"valid":True,
             },
             {"word": "أولاد",  # kitab
-             "vocalized": "أَوْلاَدٌ", "number": "جمع تكسير", "gender": "مذكر",  "defined":False,"valid": True,
+             "vocalized": "أَوْلاَدٌ", "number": "جمع تكسير", "gender": GENDER_MALE,  "defined":False,"valid": True,
              },
         ]
         attr_list = ["vocalized", "number","gender", "defined"]
@@ -293,6 +180,124 @@ class TestPhrasePattern(unittest.TestCase):
             # check transitivity validity as expected
             self.assertEqual(trans == out_trans, item["valid_trans"],
                              msg=f"Verb:'{verb}', Transitivity: expected:'{trans}', found:'{out_trans}'")  # Ensure future type is returned correctly
+
+    def test_add_components_valid(self):
+        components_list =[ {"id":1, "components":{'subject': 'هو',
+                                    'object': 'كِتَابٌ',
+                                    'verb': 'قَرَأَ',
+                                    'time': 'كُلَّ يَوْمٍ',
+                                    'place': 'غُرْفَةٌ',
+                                    'tense': TenseFuture,
+                                    'voice': ACTIVE_VOICE,
+                                    'auxiliary': 'أَرَادَ',
+                                    'negative': AFFIRMATIVE,
+                                    'phrase_type': VERBAL_PHRASE},
+                            "valid":True,
+                    },
+                    {"id": 2, "components": {'subject': 'هو',
+                                            'object': 'كِتَابٌ',
+                                            'verb': 'قَرَأَ',
+                                            'time': 'كُلَّ يَوْمٍ',
+                                            'place': 'غُرْفَةٌ',
+                                            'tense': TenseFuture,
+                                            'voice': ACTIVE_VOICE,
+                                            'auxiliary': 'أَرَادَ',
+                                            'negative': AFFIRMATIVE,
+                                            'phrase_type': VERBAL_PHRASE},
+                    "valid": True,
+                     },
+
+                    {"id": 3, "components": {'subject': 'هو',
+                                            'object': 'كِتَابٌ',
+                                            'verb': 'قَرَأَ',
+                                            'time': 'كُلَّ يَوْمٍ',
+                                            'place': 'غُرْفَةٌ',
+                                            'tense': TenseFuture,
+                                            'voice': ACTIVE_VOICE,
+                                            'auxiliary': 'أَرَادَ',
+                                            'negative': AFFIRMATIVE,
+                                            #'phrase_type': VERBAL_PHRASE
+                                              },
+                    "valid": False,
+                    },
+                    {"id": 4, "components": {'subject': 'هو',
+                                            'object': 'كِتَابٌ',
+                                            'verb': 'قَرَأَ',
+                                            'time': 'كُلَّ يَوْمٍ',
+                                            'place': 'غُرْفَةٌ',
+                                            'tense': TenseFuture,
+                                            'voice': ACTIVE_VOICE,
+                                            'auxiliary': 'أَرَادَ',
+                                            'negative': AFFIRMATIVE,
+                                            'phrase_type': VERBAL_PHRASE,
+                                            'wrong_name': 'حقل غير معروف'
+                                              },
+                    "valid": False,
+                    },
+            ]
+        for item in components_list:
+            result = self.phrase.add_components(item["components"])
+            self.assertEqual(result > 0,item["valid"], msg=f"add components \nnames{self.phrase.nodes}\nfeatures{self.phrase.phrase_features}")  # Should return True if preparation is successful
+
+    # @unittest.skip("Test later")
+    def test_prepare_valid(self):
+        components_list =[ {"id":1, "components":{'subject': 'هو',
+                                    'object': 'كِتَابٌ',
+                                    'verb': 'قَرَأَ',
+                                    'time': 'كُلَّ يَوْمٍ',
+                                    'place': 'غُرْفَةٌ',
+                                    'tense': TenseFuture,
+                                    'voice': ACTIVE_VOICE,
+                                    'auxiliary': 'أَرَادَ',
+                                    'negative': AFFIRMATIVE,
+                                    'phrase_type': VERBAL_PHRASE},
+                            "valid":True,
+                    },
+                    {"id": 2, "components": {'subject': 'هو',
+                                            'object': 'كِتَابٌ',
+                                            'verb': 'قَرَأَ',
+                                            'time': 'كُلَّ يَوْمٍ',
+                                            'place': 'غُرْفَةٌ',
+                                            'tense': TenseFuture,
+                                            'voice': ACTIVE_VOICE,
+                                            'auxiliary': 'أَرَادَ',
+                                            'negative': AFFIRMATIVE,
+                                            'phrase_type': VERBAL_PHRASE},
+                    "valid": True,
+                    },
+            ]
+
+
+
+        for item in components_list:
+            self.phrase.add_components(item["components"])
+            result = self.phrase.prepare()
+            self.assertTrue(result)  # Should return True if preparation is successful
+
+    # @unittest.skip("Test later")
+    def test_build(self):
+        test_set=[ {"id":1,
+                    "components":{'subject': 'تَاجِرٌ', 'object': '', 'verb': 'سَافَرَ', 'time': 'البَارِحَةَ', 'place': 'طَرِيقٌ',
+                       'tense': TensePast, 'voice': PASSIVE_VOICE, 'auxiliary': 'اِسْتَطَاعَ',
+                       'negative': NEGATIVE, 'phrase_type': NOMINAL_PHRASE},
+                     "phrase":"التَّاجِرُ لَمْ يُسْتَطَعْ أَنْ يُسَافَرَ فِي الطَّرِيقِ البَارِحَةَ",
+                    "valid":True,
+                    },
+                    {"id":1,
+                    "components":{'subject': 'مُهَنْدِسٌ', 'object': '', 'verb': 'اشْتَرَى', 'time': 'أَمْسِ',
+                                  'place': 'سُوقٌ', 'tense': TensePast, 'voice': ACTIVE_VOICE,
+                                  'auxiliary': '', 'negative': NEGATIVE, 'phrase_type': VERBAL_PHRASE},
+                      "phrase":"لَمْ يَشْتَرِ الْمُهَنْدِسُ فِي السُّوقِ أَمْسِ",
+                      "valid":False,
+                    },
+        ]
+        for item in test_set:
+            self.phrase.add_components(item["components"])
+            self.phrase.prepare()
+            result = self.phrase.build()
+            self.assertEqual(result == item["phrase"], item["valid"],
+                            msg= f"\nResult  :{result}\nExpected:{item['phrase']}")  # Should return True if preparation is successful
+
 
 if __name__ == '__main__':
     unittest.main()
